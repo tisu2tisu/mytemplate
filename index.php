d3c301c672d70fc259402e53e9cfec17ca2eb8ba
root <root@localhost.tisubasah>
2014-09-04 16:42:58 +0700

    My Template

---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> tisubasah | learn from basic and become expert.</title>
+			
+			<style type="text/css">
+
+				body
+					{
+						background: #efefef;
+						margin: 0px;
+						padding: 0px;
+					}
+				#container
+					{
+						margin: 0;
+						padding: 0;
+					}
+				#header
+					{
+						background-color: #737495;
+						color: #ffffff;
+						width: 100%;
+						padding: 30px;
+					}
+
+				#header p 
+					{
+						font-size: 12px;
+						position: absolute;
+						top: 65px;
+						left: 40px;
+					}
+
+				#header h1
+					{
+						position: absolute;
+						left: 30px;
+						top: 27px;
+						margin: 0;
+						padding: 0;
+						font-size: 50px;
+					}
+
+				#header ul
+					{
+						padding: 0;
+						margin-top: 25px;
+						margin-left: 270px;
+					}
+
+				#header li
+					{
+						list-style-type: none;
+						display: inline;
+						margin: 0;
+					}
+
+				#header li a 
+					{
+						padding-top: 7px;
+						padding-right: 20px;
+						padding-bottom: 7px;
+						text-decoration: none;
+						color: rgba(255, 255, 255, 4);
+						margin-left: 10px;
+						padding-left: 20px;
+						border-radius: 5px;
+						background-color: #f17d80;
+					}
+
+				#header li a:hover
+					{
+						background: #e0666a;
+					}
+
+				#contentContainer
+					{
+						margin: 0;
+						padding: 0;
+					}
+
+				#content
+					{
+						color: #666666;
+						margin-top: 40px;
+						margin-left: 20px;
+						margin-bottom: 10px;
+						margin-right: 20px;
+						width: 416px;
+						padding-top: 15px;
+						padding-right: 20px;
+						padding-left: 20px;
+						border-radius: 5px;
+						background-color: #ffffff;
+						padding: 15px 20px 20px 20px;
+						display: block;
+					}
+
+				#content h2
+					{
+						margin-top: 10px;
+						margin-bottom: 20px;
+						padding: 0;
+
+					}
+
+				#content p
+					{
+						padding-top: 0;
+						padding-right: 0;
+						padding-left: 0;
+						line-height: 20px;
+						font-size: 12px;
+					}
+
+				#content a
+					{
+						font-family: Arial, Helvetica, sans-serif;
+						color: #666666;
+						text-decoration: none;
+						padding-top: 6px;
+						padding-right: 10px;
+						padding-bottom: 6px;
+						padding-left: 10px;
+						border-radius: 3px;
+						width: 60px;
+						margin-top: 20px;
+						display: block;
+						font-size: 12px;
+						border: 1px solid #d5d5d5;
+						background-color: #fafafa;
+					}
+
+				#content a:hover
+					{
+						background: #dfdfdf;
+					}
+
+				#content:hover
+					{
+						background-color: #f7f7f7;
+					}
+			</style>
+		</head>
+		<body>
+			<div id="container">
+
+				<div id="header">
+					<h1> Tisubasah </h1>
+						<p> Learning from basic and become expert </p>
+					<ul>
+						<li>
+							<a href="#">HOME</a>
+							<a href="#">Programming</a>
+							<a href="#">Animasi</a>
+							<a href="#">Philosophy</a>
+							<a href="#">About me </a>
+						</li>
+					</ul>
+			 	</div>
+				
+			 	<div id="contentContainer">
+			 		<div id="content">
+			 			<h2> [Tuts] Mysql for beginners </h2>
+			 			<p>
+			 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit rhoncus enim at lacinia. Quisque vulputate, ex et elementum molestie, dui nisl euismod metus, nec dignissim nulla tortor a velit. Mauris ultricies odio luctus, molestie arcu vel, egestas diam. Maecenas leo magna, iaculis eget elementum vitae, varius vitae lorem. Cras gravida nibh ac velit tincidunt, sit amet cursus nisl faucibus. Nulla porttitor tincidunt enim vel dignissim. Sed accumsan consequat augue vitae cursus. Quisque vulputate malesuada lectus at dapibus.
+						</p>
+						<a href="#"> Read More </a>
+			 		</div>
+
+			 		<div id="content">
+			 			<h2> Hello world ! </h2>
+			 			<p>
+			 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec blandit rhoncus enim at lacinia. Quisque vulputate, ex et elementum molestie, dui nisl euismod metus, nec dignissim nulla tortor a velit. Mauris ultricies odio luctus, molestie arcu vel, egestas diam. Maecenas leo magna, iaculis eget elementum vitae, varius vitae lorem. Cras gravida nibh ac velit tincidunt, sit amet cursus nisl faucibus. Nulla porttitor tincidunt enim vel dignissim. Sed accumsan consequat augue vitae cursus. Quisque vulputate malesuada lectus at dapibus.
+						</p>
+						<a href="#"> Read More </a>
+			 		</div>
+				</div>
+
+			</div>
+		</body>
+	</html> 
